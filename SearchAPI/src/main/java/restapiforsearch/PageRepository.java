20b889fc2ac500d2c053517549f320d72bbc8b3a
MuxaJlbl4 <muxbl4@gmail.com>
2016-07-07 16:43:59 +0300

    updated

--- SearchAPI/src/main/java/restapiforsearch/PageRepository.java
+++ SearchAPI/src/main/java/restapiforsearch/PageRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+@RepositoryRestResource(collectionResourceRel = "page", path = "page")
 public interface PageRepository extends MongoRepository<Page, String> {
 
-    List<Page> findByLastName(@Param("name") String name);
+    List<Page> findByUrl(@Param("name") String name);
 
 }
